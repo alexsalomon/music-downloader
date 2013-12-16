e8fd58ba83a861dce8d54fbaff569ae102dd17fa
Alex Salomon <alexsalomon.th@gmail.com>
2013-12-15 20:12:04 -0800

    Added support to download full playlists

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 Music Downloader
 ================
 
-Application that downloads songs from YouTube given a video URL.
+Application that downloads songs from YouTube given a video or playlist  URL.\n
+In the case of a playlist, all the songs will be downloaded.\n
 Tested only on Mac OS X 10.8.5.
 
 Set up
@@ -20,8 +21,12 @@ chmod u+x music_downloader.py
 Downloading Songs:
 ------------------
 
-You can download a song from YouTube by providing the script with a URL of a video containing the song:
-
+You can download a song from YouTube by providing an URL of a video containing the song:
 ```
 ./music_downloader.py "http://www.youtube.com/watch?v=r0a-o16i_Gw"
+```
+
+You can also download all the songs on a playlist by providing the playlist URL with your YouTube username and password:
+```
+./music_downloader.py -u myusername@gmail.com -p mypassword123 "http://www.youtube.com/playlist?list=PLLMgg-xBAzEwEIDofx_nGPApLDIL5IkUx"
 ```
--- music_downloader.py
+++ music_downloader.py
@@ -7,39 +7,61 @@ def main():
 	parser = getInputParser()
 	args = parser.parse_args()
 
-	downloadYoutubeSong(args.song_url)
+	if validateArguments(args):
+		downloadYoutubeSongs(args.url)
 
 def getInputParser():
 	parser = ArgumentParser()
 	parser.add_argument(
-		"song_url",
-		help="The URL of a song to be downloaded.",
+		"url",
+		help="The URL of a song or a playlist on YouTube to be downloaded.",
 	)
+	parser.add_argument(
+		'-u',
+		dest='username',
+		default=None,
+        help="The user's YouTube username [required when downloading playlists]."
+	)
+	parser.add_argument(
+		'-p',
+		dest='password',
+		default=None,
+        help="The user's YouTube password [required when downloading playlists]."
+	)		
 
 	return parser
 
-def downloadYoutubeSong(url):	
-	if validateYoutubeURL(url):
-		print "Downloading song..."
-		call([
-			"youtube-dl",
-			"--restrict-filenames",
-			"-o Downloaded Songs/%(title)s.%(ext)s", #output file name and path
-			"--format=22/18",
-			"--min-filesize=50k",			
-			"--no-overwrites",
-			"--no-part",
-			"--extract-audio",
-			"--audio-quality=0", #best quality
-			"--audio-format=mp3",
-			url
-		])
-		print "Song successfully downloaded."
-	else:
-		print "Invalid URL."
+def downloadYoutubeSongs(url):	
+	''' Function that downloads a video in the MP4 format from YouTube and converts it to MP3.
+	The url parameter can be an URL for a single video or a public playlist.
+	In the case of a playlist, the whole playlist will be downloaded. 
+	'''
+	print "Downloading song..."
+	call([
+		"youtube-dl",
+		"--restrict-filenames",
+		"-o Downloaded Songs/%(title)s.%(ext)s", #output file name and path
+		"--format=22/18",
+		"--min-filesize=50k",			
+		"--no-overwrites",
+		"--extract-audio",
+		"--audio-quality=0", #best quality
+		"--audio-format=mp3",
+		url
+	])
+	print "Song successfully downloaded."
 
-def validateYoutubeURL(url):
-	return "youtube" in url
+def validateArguments(args):
+	isValid = True
+
+	if "youtube" not in args.url:
+		print "*** You must insert a valid YouTube URL."
+		isValid = False
+	elif "playlist" in args.url and (args.username is None or args.password is None):
+		print "*** You must specify your YouTube username and password to download a playlist."
+		isValid = False
+
+	return isValid
 
 if __name__ == "__main__":
 	main()
